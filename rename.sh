ee24257491f70ba06d805458c6c09f6cab2dbeeb
esrrhs <esrrhs@163.com>
2021-03-11 06:16:51 +0000

    add

--- rename.sh
+++ rename.sh
@@ -9,3 +9,15 @@ find ./data -type f -name '*' | while read FILE; do
   fi
   echo "done "${FILE}
 done
+
+find ./data -type f -name '*' | while read FILE; do
+  md5=$(md5sum "${FILE}" | awk '{print $1}')
+  ext="$(echo ${FILE} | grep -o "\.[a-zA-Z]*$")"
+  for name in $(ls -l ./data | grep $md5 | awk '{print $9}'); do
+    other_ext="$(echo ${name} | grep -o "\.[a-zA-Z]*$")"
+    if [ "$ext" != "$other_ext" ]; then
+      echo "dumplicate "${FILE}
+      rm -f ${FILE}
+    fi
+  done
+done
