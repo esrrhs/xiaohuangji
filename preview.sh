f2d02a50be6254a703a13af914ed21b3c9daaf59
esrrhs <esrrhs@163.com>
2025-12-30 09:43:22 +0800

    add

--- preview.sh
+++ preview.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-echo "" >preview.md
+cp README.template README.md
 
 find ./data -type f -name '*' | while read FILE; do
-  echo -n "<img src=\"$FILE\" width=\"100\" /> " >> preview.md
+  echo -n "<img src=\"$FILE\" width=\"100\" /> " >> README.md
 done
